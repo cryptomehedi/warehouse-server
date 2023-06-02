aaa0bd53bf4020958865ef44753a97ae2ee114a3
Mehedi Hasan <classicalmehedi@gmail.com>
2023-06-02 13:09:42 +0600

    test v2-1

--- index.js
+++ index.js
@@ -30,9 +30,10 @@ function JWTAccess(req, res, next) {
 // mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mkpap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-    try {
-        await client.connect()
+// async function run(){
+//     try {
+        // await client.connect()
+        client.connect()
         const productCollection = client.db('warehouse').collection('product')
 
         app.get('/stockAllPd', async (req, res) =>{
@@ -131,12 +132,12 @@ async function run(){
             const result = await productCollection.deleteOne(query)
             res.send(result)
         })
-    }
-    finally{
+//     }
+//     finally{
         
-    }
-}
-run().catch(console.dir)
+//     }
+// }
+// run().catch(console.dir)
 
 
 
